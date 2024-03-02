0e04d6d9722ac71e4e60af5790944bf909a3b0f5
MarcoAndretti <mrodriguezm32@miumg.edu.gt>
2024-03-02 10:30:34 -0600

    ejercicio 2.2 realizado por Marco Andretti Rodriguez Motta 0909-23-2731

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    int a = 0;
+   /* int a = 0;
     int b = 0;
 
     for (int i = 0; i <= 50; i++) {
@@ -16,6 +16,14 @@ int main() {
 
     std::cout << "la suma de los numeros impares de la variable a son: " << a << "\n";
     std::cout << "la suma de los numeros pares numeros pares de la variable b son: " << b << "\n";
+*/
+
+  int  numero = 0;
+
+    while (numero <= 30 && numero % 2 == 0) {
+        numero += 3;
+    }
+    std::cout << "El primer numero impar despues de 30 es: " << numero << "\n";
 
 
     return 0;
